b6e7abcbab6fbe5a6fa2281c5f5aa4d85fc6234a
idankario <idankario@gmail.com>
2022-10-02 16:35:28 +0300

    add entity framework and update bak file

---
+++ server/VehicleFleet/Controllers/VehiclesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VehicleFleet.Data;
+
+namespace VehicleFleet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly VehicleFleetContext _context;
+
+        public VehiclesController(VehicleFleetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vehicles
+        [HttpGet]
+        public JsonResult GetVehicles() => new(_context.Vehicles);
+
+        // GET: api/Vehicles/5
+        [HttpGet("{licensePlate}")]
+        public async Task<ActionResult<Vehicle>> GetVehicle(int licensePlate)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(licensePlate);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return vehicle;
+        }
+
+        // PUT: api/Vehicles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{licensePlate}")]
+        public  JsonResult PutVehicle(string licensePlate, Vehicle vehicle)
+        {
+            var result = _context.Vehicles.Where(x => x.LicensePlate == licensePlate).First(); 
+            if (result != null)
+            {
+                try
+                {
+                    vehicle.VehicleId = result.VehicleId; //This is important when we first create an object (NewObj), in which the default Id = 0. We can not change an existing key.
+                    _context.Entry(result).CurrentValues.SetValues(vehicle);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    return new JsonResult("Error");
+                }
+               
+            }
+            else
+                return new JsonResult("Error");
+
+            return new JsonResult("Success");
+        }
+
+        // POST: api/Vehicles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+         
+        public  JsonResult PostVehicle(Vehicle vehicle)
+        {
+            _context.Vehicles.Add(vehicle);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch 
+            {
+
+                return new JsonResult("Error");
+            }
+             
+
+            return new(_context.Vehicles);
+        }
+
+        // DELETE: api/Vehicles/5
+        [HttpDelete("{licensePlate}")]
+        public async Task<IActionResult> DeleteVehicle(string licensePlate)
+        {
+            var vehicle =  _context.Vehicles.SingleOrDefault(x => x.LicensePlate == licensePlate);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vehicles.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VehicleExists(string licensePlate)
+        {
+            return _context.Vehicles.Any(e => e.LicensePlate == licensePlate);
+        }
+    }
+}
---
+++ server/VehicleFleet/Data/VehicleFleetContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace VehicleFleet.Data
+{
+    public partial class VehicleFleetContext : DbContext
+    {
+        public VehicleFleetContext()
+        {
+        }
+
+        public VehicleFleetContext(DbContextOptions<VehicleFleetContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Vehicle> Vehicles { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Vehicle-Fleet;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.ToTable("Vehicle");
+
+                entity.HasIndex(e => e.LicensePlate, "UQ__Vehicle__5BC9DE416BB290C9")
+                    .IsUnique();
+
+                entity.Property(e => e.Color)
+                    .HasMaxLength(20)
+                    .HasColumnName("color");
+
+                entity.Property(e => e.LicensePlate)
+                    .HasMaxLength(20)
+                    .HasColumnName("licensePlate");
+
+                entity.Property(e => e.Manufacturers)
+                    .HasMaxLength(20)
+                    .HasColumnName("manufacturers");
+
+                entity.Property(e => e.Model)
+                    .HasMaxLength(20)
+                    .HasColumnName("model");
+
+                entity.Property(e => e.Year)
+                    .HasColumnType("datetime")
+                    .HasColumnName("year");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
